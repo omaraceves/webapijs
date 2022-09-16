eeb6a9f65827d4b52606001d8827580d11f2325f
omaraceves <lcc.omar.aceves@gmail.com>
2022-09-16 10:30:18 -0700

    Update model migration

--- Migrations/20220916172748_modelupdate.Designer.cs
+++ Migrations/20220916172748_modelupdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserDeviceApi.Context;
 
@@ -11,9 +12,11 @@ using UserDeviceApi.Context;
 namespace UserDeviceApi.Migrations
 {
     [DbContext(typeof(UserDevicesDB))]
-    partial class UserDevicesDBModelSnapshot : ModelSnapshot
+    [Migration("20220916172748_modelupdate")]
+    partial class modelupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220916172748_modelupdate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserDeviceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserDevices_UserDeviceCodes_UserDeviceCodeId1",
+                table: "UserDevices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserDevices_UserDeviceCodeId1",
+                table: "UserDevices");
+
+            migrationBuilder.DropColumn(
+                name: "UserDeviceCodeId",
+                table: "UserDevices");
+
+            migrationBuilder.DropColumn(
+                name: "UserDeviceCodeId1",
+                table: "UserDevices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDeviceCodes_UserDeviceId",
+                table: "UserDeviceCodes",
+                column: "UserDeviceId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserDeviceCodes_UserDevices_UserDeviceId",
+                table: "UserDeviceCodes",
+                column: "UserDeviceId",
+                principalTable: "UserDevices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserDeviceCodes_UserDevices_UserDeviceId",
+                table: "UserDeviceCodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserDeviceCodes_UserDeviceId",
+                table: "UserDeviceCodes");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserDeviceCodeId",
+                table: "UserDevices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserDeviceCodeId1",
+                table: "UserDevices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDevices_UserDeviceCodeId1",
+                table: "UserDevices",
+                column: "UserDeviceCodeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserDevices_UserDeviceCodes_UserDeviceCodeId1",
+                table: "UserDevices",
+                column: "UserDeviceCodeId1",
+                principalTable: "UserDeviceCodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
